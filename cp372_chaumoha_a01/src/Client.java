080d1c0a3576e7022dd4d84d8cc15a7e05a5fa48
Bryan Chau <chau3120@mylaurier.ca>
2014-01-20 17:39:38 -0500

    Client stuff

--- cp372_chaumoha_a01/src/Client.java
+++ cp372_chaumoha_a01/src/Client.java
@@ -1,4 +1,26 @@
+import java.io.*;
+import java.net.*;
 
 public class Client {
+	public static void main(String[] args) throws IOException {
 
+        Socket socket = null;
+        PrintWriter out = null;
+        BufferedReader in = null;
+        
+        try{
+        	socket = new Socket("192.168.5.6",80);
+        	//TODO: do we need a buffer?
+        }
+        catch(UnknownHostException e){
+        	//TODO: display error on GUI about unknown host
+        	System.exit(1);
+        }
+        catch(IOException e){
+        	//TODO: display error on GUI about I/O
+        	System.exit(1);
+        }
+        
+        
+	}
 }

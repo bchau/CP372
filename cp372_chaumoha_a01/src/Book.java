bf2fe8bbd94dd3a395c26ca9dd756801fa15967b
Bryan Chau <chau3120@mylaurier.ca>
2014-02-05 18:42:24 -0500

    Commenting most server side stuff

--- cp372_chaumoha_a01/src/Book.java
+++ cp372_chaumoha_a01/src/Book.java
@@ -1,34 +1,66 @@
 public class Book{
 		private String title, author, location;
 
+		/**
+		 * 
+		 * @return book title
+		 */
 		public String getTitle() {
 			return title;
 		}
 
+		/**
+		 * 
+		 * @param title Book title
+		 */
 		public void setTitle(String title) {
 			this.title = title;
 		}
 
+		/**
+		 * 
+		 * @return book author
+		 */
 		public String getAuthor() {
 			return author;
 		}
 
+		/**
+		 * 
+		 * @param author Book author
+		 */
 		public void setAuthor(String author) {
 			this.author = author;
 		}
 
+		/**
+		 * 
+		 * @return book location
+		 */
 		public String getLocation() {
 			return location;
 		}
 
+		/**
+		 * 
+		 * @param location Book location
+		 */
 		public void setLocation(String location) {
 			this.location = location;
 		}
 		
+		/**
+		 * 
+		 * @return True if at least one of the books attributes are null
+		 */
 		public boolean hasNull(){
 			return location == null || author == null || title == null;
 		}
 		
+		/**
+		 * 
+		 * @return True if all of the books attributes are null
+		 */
 		public boolean allNull(){
 			return location == null && author == null && title == null;
 		}

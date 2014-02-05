c3fe84c322076a3495b6a74ebe4fe9b1d1765eb2
mmtaki <moha7220@mylaurier.ca>
2014-02-05 18:18:48 -0500

    Fixed server to be persistent. No longer throws the null pointer

--- cp372_chaumoha_a01/src/Server.java
+++ cp372_chaumoha_a01/src/Server.java
@@ -6,71 +6,76 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 
-
 public class Server {
 
 	private static int portnum = 4444;
-	
-	public static void main(String[] args) throws IOException{
+
+	public static void main(String[] args) throws IOException {
 		ServerSocket serverSocket = null;
-		
-		try{
-			portnum = Integer.parseInt(args[0]); 
+
+		try {
+			portnum = Integer.parseInt(args[0]);
+		} catch (Exception e) {
 		}
-		catch(Exception e){}
-		
-		while(true){
-			try{
+
+		while (true) {
+			try {
 				serverSocket = new ServerSocket(portnum);
-			}
-			catch(IOException e){
+			} catch (IOException e) {
 				System.err.println("Could not listen on port 4444.");
 				System.exit(1);
 			}
 
 			Socket clientSocket = null;
-			try{
+			try {
 				clientSocket = serverSocket.accept();
 				System.out.println("Accepted");
-			}
-			catch (IOException e){
+			} catch (IOException e) {
 				System.err.println("Accept failed.");
 				System.exit(1);
 			}
 
-			PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-			BufferedReader in = new BufferedReader(
-					new InputStreamReader(
-							clientSocket.getInputStream()));
+			PrintWriter out = new PrintWriter(clientSocket.getOutputStream(),
+					true);
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					clientSocket.getInputStream()));
 			String inputLine, outputLine;
 			Protocol protocol = new Protocol();
 
 			int code = -1;
-			while ((inputLine = in.readLine()) != null || code != -1){	
-				if (inputLine.startsWith("SUBMIT")){
-					code = Protocol.SUBMIT;
-				}
-				else if (inputLine.startsWith("GET")){
-					code = Protocol.GET;
-				}
-				else if (inputLine.startsWith("REMOVE")){
-					code = Protocol.REMOVE;
-				}
-				else{
-					code = Protocol.FAULT;
-				}
+			while ((inputLine = in.readLine()) != null || code != -1) {
+				try {
+					if (inputLine.startsWith("SUBMIT")) {
+						code = Protocol.SUBMIT;
+					} else if (inputLine.startsWith("GET")) {
+						code = Protocol.GET;
+					} else if (inputLine.startsWith("REMOVE")) {
+						code = Protocol.REMOVE;
+					} else {
+						code = Protocol.FAULT;
+					}
 
-				String temp;
-				while (in.ready() && !Protocol.isKeyword(temp = in.readLine())){  
-					inputLine += " "+temp;
+					String temp;
+					while (in.ready()
+							&& !Protocol.isKeyword(temp = in.readLine())) {
+						inputLine += " " + temp;
+					}
+
+					outputLine = protocol.processInput(code, inputLine);
+					out.println(outputLine);
+					
+				} catch (NullPointerException e) { // on error wait on connection
+					System.out.println("Client was disconnected.");
+					try { // wait on the socket to get data
+						clientSocket = serverSocket.accept();
+						out = new PrintWriter(clientSocket.getOutputStream(),
+								true);
+						in = new BufferedReader(new InputStreamReader(
+								clientSocket.getInputStream()));
+					} catch (IOException ioe) {
+						System.err.println("Unable to accept new connection.");
+					}
 				}
-
-				outputLine = protocol.processInput(code, inputLine);
-				out.println(outputLine);
-				if (outputLine.equals("Bye."))
-					break;
-
-
 			}
 			serverSocket.close();
 		}
